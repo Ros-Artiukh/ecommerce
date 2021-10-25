2f21e5b36efc6e94a894ff071db0e8d2c921f2da
Rostislav Artiukh <r.artiukh97@gmail.com>
2021-10-25 22:48:51 +0300

    rename Main

--- ECommerce.Api.Orders/Program.cs
+++ ECommerce.Api.Orders/Program.cs
@@ -11,7 +11,7 @@ namespace ECommerce.Api.Orders
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main1(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
